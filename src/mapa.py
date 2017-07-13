6ecd6b2ed4d487e20e6d5fbefee34fc920b38e2e
Marcos Andre <marcos@getrak.com>
2017-07-13 10:19:37 -0300

    finalizado conversao para json

--- src/mapa.py
+++ src/mapa.py
@@ -6,44 +6,22 @@ import re
 import codecs
 import json
 
-
-def getAdress(subtag, address_type):
-    if address_type == 'street':
-        logradouro = subtag.get('v')
-        logradouro_mapping = { "Av":"Avenida", "Av.":"Avenida", "R":"Rua", "R.":"Rua",  "rua":"Rua",
-                           "r.":"Rua", "r":"Rua", "Pr.":u"Praça", "PR.":u"Praça"}
-                           
-        #Compila a expressão regular "^\S+\.?". Incio da string até algum espaço ou ponto "."
-        logradouro_regex = re.compile(r'^\S+\.?',re.IGNORECASE)
-    
-        # Faz uma varredura através do string logradouro procurando a primeira parte da string logradouro 
-        # que bate com o padrão, parte inicial do logradouro até algum espaço ou ponto.
-        match  = logradouro_regex.search(logradouro)
-        if match:
-            nome_inicial = match.group(0)
-            if nome_inicial in logradouro_mapping.keys():
-                logradouro = re.sub(logradouro_regex, logradouro_mapping[nome_inicial], logradouro)
-          
-        return logradouro
-    else:
-        return subtag.get('v')
-        
 def parser_subtag(element, node):
     address = {}
     for subtag in element:
         if subtag.tag == 'tag': 
             if subtag.get('k').startswith('addr:'):
-                address_type = subtag.get('k')[5:]
-                address[address_type] = getAdress( subtag, address_type )
+                address[subtag.get('k')[5:]] = subtag.get('v')
             else:
                 node[subtag.get('k')] = subtag.get('v') 
         
     if len(address) > 0:
         node['address'] = address
+        print address
         
     return node
     
-def shape_element(element):
+def parser_element(element):
     node = {}
     
     if element.tag == "node" or element.tag == "way" :
@@ -55,16 +33,32 @@ def shape_element(element):
     else:
         return None
 
+def xml_to_json(arquivo_entrada, arquivo_saida):
+    
+    with codecs.open(arquivo_saida, "w") as saida:
+        saida.write("[");
+        start = True
+        for _, element in ET.iterparse(arquivo_entrada):
+            el = parser_element(element)
+            if el:
+                if start == False:
+                     saida.write( ",\n" )
+                     
+                saida.write(json.dumps(el))
+                start = False
+                
+        saida.write("]");
+    
 def process(file):   
     data = []
     for _, element in ET.iterparse(file):
-            el = shape_element(element)
-            if el:
-                data.append(el)
-                
+        el = parser_element(element)
+        if el:
+            data.append(el)
+         
     return data
     
 
 file_path  = "../data/bhmicro"
-data = process( file_path + ".osm")
-print data
+data = xml_to_json( file_path + ".osm", file_path + ".json")
+#print data
