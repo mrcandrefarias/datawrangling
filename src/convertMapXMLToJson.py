a187a925b2136fc0c0118db7ff460310154f43b5
Marcos Andre <marcos@getrak.com>
2017-07-13 10:49:27 -0300

    ajustes no script

--- src/convertMapXMLToJson.py
+++ src/convertMapXMLToJson.py
@@ -6,59 +6,39 @@ import re
 import codecs
 import json
 
-def parser_subtag(element, node):
+def parser_subtag(element, documento):
     address = {}
     for subtag in element:
         if subtag.tag == 'tag': 
             if subtag.get('k').startswith('addr:'):
                 address[subtag.get('k')[5:]] = subtag.get('v')
             else:
-                node[subtag.get('k')] = subtag.get('v') 
+                documento[subtag.get('k')] = subtag.get('v') 
         
     if len(address) > 0:
-        node['address'] = address
+        documento['address'] = address
         print address
         
-    return node
+    return documento
     
 def parser_element(element):
-    node = {}
-    
+    documento = {}
     if element.tag == "node" or element.tag == "way" :
         for e in element.attrib.keys():    
-            node[e]      = element.get(e)
-            node['type'] = element.tag
-            
-        return parser_subtag(element, node)
+            documento[e]      = element.get(e)
+            documento['type'] = element.tag
+         
+        return parser_subtag(element, documento)
     else:
         return None
 
 def xml_to_json(arquivo_entrada, arquivo_saida):
-    
     with codecs.open(arquivo_saida, "w") as saida:
-        saida.write("[");
-        start = True
         for _, element in ET.iterparse(arquivo_entrada):
             el = parser_element(element)
-            if el:
-                if start == False:
-                     saida.write( ",\n" )
-                     
-                saida.write(json.dumps(el))
-                start = False
+            if el:     
+                saida.write( json.dumps(el) + "\n")
                 
-        saida.write("]");
-    
-def process(file):   
-    data = []
-    for _, element in ET.iterparse(file):
-        el = parser_element(element)
-        if el:
-            data.append(el)
-         
-    return data
-    
 
 file_path  = "../data/bhmicro"
-data = xml_to_json( file_path + ".osm", file_path + ".json")
-#print data
+xml_to_json( file_path + ".osm", file_path + ".json")
