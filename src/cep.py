3f1abaad38a892332686b9290b79e599c4924700
Marcos Andre <marcos@getrak.com>
2017-07-14 17:59:46 -0300

    add cep

--- src/cep.py
+++ src/cep.py
@@ -9,8 +9,8 @@ def get_db():
     return db
 
 db           = get_db()
-enderecos    = db.bh.find( {'address':{'$exists':1}}, {"address":1, "_id":1} )
-nomes_cidade = Set()
+#enderecos    = db.bh.find( {'address':{'$exists':1}}, {"address":1, "_id":1} )
+enderecos = []
 
 def is_formato_cep_invalido(cep):
     cep_regex = re.match(r'^\d{5}-\d{3}$', cep)
@@ -28,4 +28,7 @@ for endereco in enderecos:
         #db.bh.save(endereco)
     #print address
     
+my_str = "hey .123 3698"
+my_new_string = re.sub('[^0-9]', '', my_str)
+print my_new_string
 #print nomes_cidade
