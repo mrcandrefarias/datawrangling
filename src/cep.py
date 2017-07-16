5b5047db2d8e003f86c64701fe43eb0040a3305e
Marcos André <mrcandrefarias@gmail.com>
2017-07-16 11:17:41 -0300

    atualizado dados

--- src/cep.py
+++ src/cep.py
@@ -8,27 +8,34 @@ def get_db():
     db     = client['bh-osm']
     return db
 
-db           = get_db()
-#enderecos    = db.bh.find( {'address':{'$exists':1}}, {"address":1, "_id":1} )
-enderecos = []
-
 def is_formato_cep_invalido(cep):
     cep_regex = re.match(r'^\d{5}-\d{3}$', cep)
     if None == cep_regex:
         return True
     return False
-        
-for endereco in enderecos:
-    if 'postcode' in endereco['address']:
-        if( is_formato_cep_invalido (endereco['address']['postcode']) ) :
-            print endereco['address']['postcode']
-        else:
-            print endereco['address']['postcode'] + ":"
-        
-        #db.bh.save(endereco)
+
+def format_cep(cep):
+    '''
+        Retira qualquer caracter da string cep que não seja número.
+        Apenas os números são mantidos na nova string cep, que é formada.
+    '''
+    cep = re.sub('[^0-9]', '', cep)
+    #Verificando quantos caracteres faltam para completar oito dígitos
+    caracteres_faltando = 8 - len(cep)
+    cep = '{}{}{}'.format(cep[0:5],'-', cep[5:] + '0' * caracteres_faltando)
+    print cep
+
+def verificaEnderecos():
+    db           = get_db()
+    enderecos    = db.bh.find( {'address':{'$exists':1}}, {"address":1, "_id":1} )
+    for endereco in enderecos:
+        clean = False
+        if 'postcode' in endereco['address']:
+            if( is_formato_cep_invalido (endereco['address']['postcode']) ) :
+                print "CEP invalido:" + endereco['address']['postcode']
+                endereco['address']['postcode'] = format_cep( endereco['address']['postcode'] )
+                clean = True
+        if(clean)
+            print "Limpando endereço:" + endereco
+            db.bh.save(endereco)
     #print address
-    
-my_str = "hey .123 3698"
-my_new_string = re.sub('[^0-9]', '', my_str)
-print my_new_string
-#print nomes_cidade
